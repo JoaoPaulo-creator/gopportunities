08858a80e47951cd1898e095b3e0fc32e1aefad5
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-26 22:19:14 -0300

    refactor: add config initializer

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/JoaoPaulo-creator/gopportunities/router"
+import (
+	"fmt"
+
+	"github.com/JoaoPaulo-creator/gopportunities/config"
+	"github.com/JoaoPaulo-creator/gopportunities/router"
+)
 
 func main() {
+	// inicializando config
+	err := config.Init()
+	// matando a aplicacao, caso de ruim no Init()
+	if err != nil {
+		fmt.Errorf("An error occured while initializating", err)
+		return
+	}
 	router.Initialize()
 }
