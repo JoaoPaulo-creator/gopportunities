488767e5430b56cdb84df2a070ef0696b962b851
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-26 22:29:45 -0300

    refactor: add custom logger

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/JoaoPaulo-creator/gopportunities/config"
 	"github.com/JoaoPaulo-creator/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
 	// inicializando config
 	err := config.Init()
 	// matando a aplicacao, caso de ruim no Init()
 	if err != nil {
-		fmt.Errorf("An error occured while initializating", err)
+		//%v serve para aceitar qualquer tipo
+		logger.Errorf("config initialization error: %v", err)
 		return
 	}
 	router.Initialize()
