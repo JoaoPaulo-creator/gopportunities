7f05a40a4f4cfabf2cdd35232ec8e97791946c22
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-31 22:13:43 -0300

    docs: add show opening swagger docs

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -8,6 +8,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary Show an opening
+// @Description shows a job opening
+// @Tags Openings
+// @Accept json
+// @Procedure json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} ShowOpeningResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [get]
 func ShowOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 
